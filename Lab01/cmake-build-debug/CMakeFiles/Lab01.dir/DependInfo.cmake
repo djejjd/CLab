2cf73388b29d5fc97bfb8413e528d00271e59136
warren <2818790266@qq.com>
2019-12-01 16:27:30 +0800

    first commit

---
+++ Lab01/cmake-build-debug/CMakeFiles/Lab01.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/warren/projects/CLionProjects/CLab/Lab01/NFAFragmentStack.c" "/home/warren/projects/CLionProjects/CLab/Lab01/cmake-build-debug/CMakeFiles/Lab01.dir/NFAFragmentStack.c.o"
+  "/home/warren/projects/CLionProjects/CLab/Lab01/OutputResult.c" "/home/warren/projects/CLionProjects/CLab/Lab01/cmake-build-debug/CMakeFiles/Lab01.dir/OutputResult.c.o"
+  "/home/warren/projects/CLionProjects/CLab/Lab01/RegexpToPost.c" "/home/warren/projects/CLionProjects/CLab/Lab01/cmake-build-debug/CMakeFiles/Lab01.dir/RegexpToPost.c.o"
+  "/home/warren/projects/CLionProjects/CLab/Lab01/main.c" "/home/warren/projects/CLionProjects/CLab/Lab01/cmake-build-debug/CMakeFiles/Lab01.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Lab01/cmake-build-debug/CMakeFiles/Lab01.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab01.dir/NFAFragmentStack.c.o"
+  "CMakeFiles/Lab01.dir/OutputResult.c.o"
+  "CMakeFiles/Lab01.dir/RegexpToPost.c.o"
+  "CMakeFiles/Lab01.dir/main.c.o"
+  "Lab01"
+  "Lab01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
