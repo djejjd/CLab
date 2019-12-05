44d04250d37c7d935656b19f628fc4bde6ba202c
Warren <2818790266@qq.com>
2019-12-05 21:29:43 +0800

    添加CMake

---
+++ cmake-build-debug/CMakeFiles/CLionProjects.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/cygdrive/d/Projects/CLionProjects/Lab01/NFAFragmentStack.c" "/cygdrive/d/Projects/CLionProjects/cmake-build-debug/CMakeFiles/CLionProjects.dir/Lab01/NFAFragmentStack.c.o"
+  "/cygdrive/d/Projects/CLionProjects/Lab01/OutputResult.c" "/cygdrive/d/Projects/CLionProjects/cmake-build-debug/CMakeFiles/CLionProjects.dir/Lab01/OutputResult.c.o"
+  "/cygdrive/d/Projects/CLionProjects/Lab01/RegexpToPost.c" "/cygdrive/d/Projects/CLionProjects/cmake-build-debug/CMakeFiles/CLionProjects.dir/Lab01/RegexpToPost.c.o"
+  "/cygdrive/d/Projects/CLionProjects/Lab01/main.c" "/cygdrive/d/Projects/CLionProjects/cmake-build-debug/CMakeFiles/CLionProjects.dir/Lab01/main.c.o"
+  "/cygdrive/d/Projects/CLionProjects/Lab04/main.c" "/cygdrive/d/Projects/CLionProjects/cmake-build-debug/CMakeFiles/CLionProjects.dir/Lab04/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Lab01"
+  "../Lab04"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CLionProjects.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CLionProjects.exe"
+  "CLionProjects.pdb"
+  "CMakeFiles/CLionProjects.dir/Lab01/NFAFragmentStack.c.o"
+  "CMakeFiles/CLionProjects.dir/Lab01/OutputResult.c.o"
+  "CMakeFiles/CLionProjects.dir/Lab01/RegexpToPost.c.o"
+  "CMakeFiles/CLionProjects.dir/Lab01/main.c.o"
+  "CMakeFiles/CLionProjects.dir/Lab04/main.c.o"
+  "libCLionProjects.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CLionProjects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
