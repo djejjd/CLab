74783b4d618d8f64fb02bf070c5c6bd197515a36
Warren <2818790266@qq.com>
2019-12-05 21:24:05 +0800

    更改Lab04

---
+++ Lab04/cmake-build-debug/CMakeFiles/Lab04.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab04.dir/main.c.o"
+  "Lab04.exe"
+  "Lab04.pdb"
+  "libLab04.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab04.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
