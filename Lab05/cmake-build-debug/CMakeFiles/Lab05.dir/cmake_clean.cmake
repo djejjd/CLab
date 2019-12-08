845866497cd6d0b47a4bcf9fe4a299c25907cfe7
warren <2818790266@qq.com>
2019-12-08 12:45:08 +0800

    添加Lab05

---
+++ Lab05/cmake-build-debug/CMakeFiles/Lab05.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab05.dir/main.c.o"
+  "Lab05"
+  "Lab05.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab05.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
